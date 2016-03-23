2946da4d720849837795b867bbc7cf46fbaf6534
Tomáš Taro <tomas@getadblock.com>
2016-03-24 00:19:55 +0100

    Update location check

--- js/master.js
+++ js/master.js
@@ -3,7 +3,7 @@
 $(function () {
     var path = document.location.pathname;
     var jsonFile = "";
-    if (path === "/pages/a.html") {
+    if (path === "/yachter-quiz/pages/a.html") {
         jsonFile = "//tomasko126.github.io/yachter-quiz/json/vmp-cevni.json";
     } else if (path === "/pages/b.html") {
         jsonFile = "//tomasko126.github.io/yachter-quiz/json/vmp-stavba-elektro.json";
