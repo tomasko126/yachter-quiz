091fbfe56b7d9dce66833a1954132b8a2e67af42
Tomáš Taro <tomas@getadblock.com>
2016-03-24 00:18:07 +0100

    Update URLs of json files

--- js/master.js
+++ js/master.js
@@ -4,11 +4,11 @@ $(function () {
     var path = document.location.pathname;
     var jsonFile = "";
     if (path === "/pages/a.html") {
-        jsonFile = "../json/vmp-cevni.json";
+        jsonFile = "//tomasko126.github.io/yachter-quiz/json/vmp-cevni.json";
     } else if (path === "/pages/b.html") {
-        jsonFile = "../json/vmp-stavba-elektro.json";
+        jsonFile = "//tomasko126.github.io/yachter-quiz/json/vmp-stavba-elektro.json";
     } else if (path === "/pages/c.html") {
-        jsonFile = "../json/vmp-plachetnice.json";
+        jsonFile = "//tomasko126.github.io/yachter-quiz/json/vmp-plachetnice.json";
     }
     $.get(jsonFile, function(data) {
         $('#slickQuiz').slickQuiz({ json: data });
