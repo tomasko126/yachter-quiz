be74c212b0650b82d2b81faea3ba5d7fb695d61a
Tomáš Taro <tomas@getadblock.com>
2016-03-30 23:59:17 +0200

    Upload PHP file containing links to files

---
+++ getLinks.php
@@ -0,0 +1,7 @@
+<?php
+    $links = array('a'=> 'tomasko126.github.io/yachter-quiz/json/vmp-cevni.json',
+                   'b'=> 'tomasko126.github.io/yachter-quiz/json/vmp-stavba-elektro.json',
+                   'c'=> 'tomasko126.github.io/yachter-quiz/json/vmp-plachetnice.json');
+
+    echo json_encode($links);
+?>
